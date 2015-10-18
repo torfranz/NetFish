c0a0fdecee95155bba49ac27d9473c980657163c
torfranz <tfranzer@gmx.de>
2015-10-18 21:18:35 +0200

    intermediate version

---
+++ NetFishTests/Types/BitboardsTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    using System.Diagnostics;
+
+    [TestClass()]
+    public class BitboardsTests
+    {
+        [TestMethod()]
+        public void initTest()
+        {
+            Bitboards.init();
+        }
+    }
+}
---
+++ PRNG.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+/// xorshift64star Pseudo-Random Number Generator
+/// This class is based on original code written and dedicated
+/// to the public domain by Sebastiano Vigna (2014).
+/// It has the following characteristics:
+///
+///  -  Outputs 64-bit numbers
+///  -  Passes Dieharder and SmallCrush test batteries
+///  -  Does not require warm-up, no zeroland to escape
+///  -  Internal state is a single 64-bit integer
+///  -  Period is 2^64 - 1
+///  -  Speed: 1.60 ns/call (Core i7 @3.40GHz)
+///
+/// For further analysis see
+///   <http://vigna.di.unimi.it/ftp/papers/xorshift.pdf>
+
+public class PRNG
+{
+        ulong s;
+
+    public ulong rand64()
+    {
+        s ^= s >> 12;
+        s ^= s << 25;
+        s ^= s >> 27;
+        return s * 2685821657736338717L;
+    }
+
+    public PRNG(ulong seed) {
+        s = seed;
+        Debug.Assert(seed != 0);
+    }
+
+    public ulong rand() { return rand64(); }
+
+    /// Special generator used to fast init magic numbers.
+    /// Output values only have 1/8th of their bits set on average.
+    public ulong sparse_rand()
+    { return (rand64() & rand64() & rand64()); }
+};
