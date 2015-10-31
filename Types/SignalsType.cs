76842a632b5e50e2c451cc79df7cf6eb5047ef5f
torfranz <tfranzer@gmx.de>
2015-10-31 19:50:57 +0100

    add SignalsType

---
+++ Types/SignalsType.cs
@@ -0,0 +1,7 @@
+﻿/// The SignalsType struct stores volatile flags updated during the search
+/// typically in an async fashion e.g. to stop the search by the GUI.
+
+public struct SignalsType
+{
+    bool stop, stopOnPonderhit, firstRootMove, failedLowAtRoot;
+};
